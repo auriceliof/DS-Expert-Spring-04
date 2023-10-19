0c19b2fc16e7b89a50d4b045bf82d533282bfd36
auriceliof <auricelio.suporte@hotmail.com>
2023-10-18 21:36:37 -0300

    04-09 Executando a consulta via método no JpaRepository

--- dscatalog-cap3/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog-cap3/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -103,7 +103,7 @@ public class ProductService {
 
 	@Transactional(readOnly = true)
 	public Page<ProductProjection> testQuery(Pageable pageable) {
-		return repository.searchProducts(Arrays.asList(1L,3L), "ma", pageable);
+		return repository.searchProducts(Arrays.asList(), "", pageable);
 	}
 }
 
