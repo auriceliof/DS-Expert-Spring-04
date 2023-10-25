1fcf4cfaebbd37acaf8c125bcfa8e8ac9d8ced59
auriceliof <auricelio.suporte@hotmail.com>
2023-10-25 09:47:04 -0300

    04-19 Caso de uso signup

--- dscatalog-cap3/backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ dscatalog-cap3/backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -47,8 +47,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(dto);
 	}
 	
-	
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
+
 	@PostMapping
 	public ResponseEntity<UserDTO> insert(@RequestBody @Valid UserInsertDTO dto) {
 		UserDTO newDto = service.insert(dto);
